5f7abb3580963c2b57e6b6f4df29dd41a47a5705
Eng-antoon <antoonkamel20000@outlook.com>
2021-08-06 17:34:06 +0200

    dfjkn

--- js/translate.js
+++ js/translate.js
@@ -17,6 +17,11 @@ i18next.init({
         'contacts':"Contact Us",
         'language':"Language",
         'linkat':"Links",
+        'adress':"2 Al Nuaimia - Ajman - United Arab Emirates.",
+        'pageend':"We would love to hear from you",
+        'footabout':"About Abedous",
+        'head1':"Mission",
+        'paramission':"Our mission is to deliver for our customers the best quality of construction services at competitive rates with highest level of professionalism and integrity resulting in long term relationships with our employees, customers, suppliers and subcontractors.",
 
 
 
@@ -38,6 +43,18 @@ i18next.init({
         'contacts':"تواصل معنا",
         'language':"اللغة",
         'linkat':"الروابط",
+        'adress':"2 النعومية - عجان - الامارات العربية المتحدة",
+        'footabout':"عن شركتنا",
+        'pageend':"يسعدنا التواصل دائما",
+        'head1':"مهمتنا",
+        'paramission':"مهمتنا هي أن نقدم لعملائنا أفضل جودة لخدمات البناء بأسعار تنافسية مع أعلى مستوى من الاحتراف والنزاهة مما يؤدي إلى علاقات طويلة الأمد مع موظفينا وعملائنا وموردينا ومقاولينا من الباطن.",
+        'head2':"رؤيتنا",
+        'paravision':"تلتزم شركة أبيدوس المحدودة ببناء علاقات طويلة الأمد مبنية على النزاهة في الأداء والقيمة ورضا العملاء. سنستمر في تلبية الاحتياجات المتغيرة لعملائنا من خلال خدماتنا عالية الجودة التي يقدمها أكثر الأشخاص المؤهلين. تتمثل رؤيتنا في أن يتم الاعتراف بها واحترامها كواحدة من الشركات الرائدة في إدارة المرافق الشاملة في الإمارات العربية المتحدة.",
+        'head3':"لماذا شركة أبيدوس للمقاولات؟",
+        'parahead1':"نحن ندرك أن لعملائنا الاختيار عند اختيار شركة إنشاءات. في شركة أبيدوس ، نسعى جاهدين لتجاوز التوقعات وتحقيق أقصى قدر من القيمة. التزام شركة أبيدوس هو لمصلحتك.",
+
+
+
 
 
         // Footer
@@ -60,3 +77,13 @@ document.querySelectorAll("a")[12].innerHTML = i18next.t('services');
 document.querySelectorAll("a")[13].innerHTML = i18next.t('projects');
 document.querySelectorAll("a")[14].innerHTML = i18next.t('contacts');
 document.querySelector("#links").innerHTML = i18next.t('linkat');
+document.querySelector("#adress").innerHTML = i18next.t('adress');
+document.querySelector("#footabout").innerHTML = i18next.t('footabout');
+document.querySelector("#pageend").innerHTML = i18next.t('pageend');
+document.querySelector("#head1").innerHTML = i18next.t('head1');
+document.querySelector("#paramission").innerHTML = i18next.t('paramission');
+document.querySelector("#head2").innerHTML = i18next.t('head2');
+document.querySelector("#paravision").innerHTML = i18next.t('paravision');
+document.querySelector("#head3").innerHTML = i18next.t('head3');
+document.querySelector("#parahead1").innerHTML = i18next.t('parahead1');
+
